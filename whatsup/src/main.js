74d52dc0967a7bee72a31da1b4a3e3ef1f56d65e
huangjundashuaige <715817451@qq.com>
2019-06-22 23:48:43 +0800

    app/main

---
+++ whatsup/src/main.js
@@ -0,0 +1,29 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import App from './App'
+import router from './router'
+import VueResource from 'vue-resource'
+import VueCookies from 'vue-cookies'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import $ from 'jquery'
+
+Vue.use(VueResource)
+Vue.use(ElementUI)
+Vue.use(VueCookies)
+library.add(faCoffee)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router,
+  components: { App },
+  template: '<App/>'
+})
